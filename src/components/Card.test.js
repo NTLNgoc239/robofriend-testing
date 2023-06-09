78ad7e81645abd13d5a84e3d223d3e542d4cf929
Ngoc.Nguyen <ntlngoc239@gmail.com>
2023-06-09 16:48:37 +0700

    test with corverage

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,6 +1,17 @@
 import { shallow, mount, render } from "enzyme";
 import React from "react";
 import Card from './Card';
-it('expect to render card component', () => {
-  expect(shallow(<Card />).length).toEqual(1)
-})
+import toJson from "enzyme-to-json";
+
+
+// it('expect to render card component', () => {
+//   const cardComponent = shallow(<Card />)
+//   expect(cardComponent.debug()).toMatchSnapshot();
+// })
+
+it("Expect to return Card component", () => {
+  const wrapper = shallow(<Card />);
+  expect(
+    toJson(wrapper)
+  ).toMatchSnapshot();
+});
